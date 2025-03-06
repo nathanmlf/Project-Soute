a569c14d52dfe7c18b358ecb698277de5c317d93
Nathan Lourenço <nathanmlf@hotmail.com>
2025-03-06 17:58:06 -0300

    finalizando o layout do site, ainda alguns problemas de dimensão e interação com o hover no ícone do github

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,46 +6,109 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <header className="header">
-      <Link className="header_link" href="/" target="_blank">
-        <Image
-          className="header_logo"
-          src="/logo.svg"
-          alt="logo"
-          width={80}
-          height={80}
-        />
-      </Link>
-      <nav className="nav">
-        <ul className="nav_items">
-          <li>
-            <Link className="nav_link" href="/">
-              Contact
-            </Link>
-          </li>
-          <li>
-            <Link className="nav_link" href="/">
-              About
-            </Link>
-          </li>
-          <li>
-            <Link className="nav_link" href="/">
-              Recipes
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <div className="github">
-        <Link href="https://github.com/nathanmlf">
+    <>
+      <header className="header">
+        <Link className="header_link" href="/" target="_blank">
           <Image
-            className="github_image"
-            src="/github.svg"
-            alt="Github logo"
-            width={60}
-            height={60}
+            className="header_logo"
+            src="/logo.svg"
+            alt="logo"
+            width={80}
+            height={80}
           />
         </Link>
-      </div>
-    </header>
+
+        <nav className="nav">
+          <ul className="nav_items">
+            <li>
+              <Link className="nav_link" href="/">
+                Contact
+              </Link>
+            </li>
+            <li>
+              <Link className="nav_link" href="/">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link className="nav_link" href="/">
+                Recipes
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="github">
+          <Link href="https://github.com/nathanmlf">
+            <Image
+              className="github-image"
+              src="/github.svg"
+              alt="Github logo"
+              width={60}
+              height={60}
+            />
+          </Link>
+        </div>
+      </header>
+      {/*  */}
+
+      <main className="main-container">
+        <div className="container-ingredients">
+          <article className="ingredients-body">
+            <section className="ingredients">
+              <h2>Ingredients</h2>
+            </section>
+
+            <section className="quantities">
+              <h2>Quantity</h2>
+            </section>
+          </article>
+        </div>
+
+        <article className="container-recipe">
+          <section className="recipe-body">
+            <h2>Recipe</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+              at purus consequat, venenatis purus dictum, sollicitudin nunc. Nam
+              lectus lectus, sodales vel ullamcorper id, interdum et nunc. Nulla
+              facilisi. Fusce ullamcorper commodo suscipit. Sed ante nulla,
+              condimentum id libero vel, vulputate condimentum arcu. Vestibulum
+              porttitor, diam vitae placerat aliquet, nisl neque pretium dolor,
+              sed facilisis tellus tellus sed turpis. Lorem ipsum dolor sit
+              amet, consectetur adipiscing elit. Sed auctor suscipit libero in
+              iaculis. Aenean ligula magna, tempor lobortis hendrerit dictum,
+              convallis ut sapien. Quisque id neque id erat tempor dapibus non
+              vel lectus.Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit. Curabitur at purus consequat, venenatis purus dictum,
+              sollicitudin nunc. Nam lectus lectus, sodales vel ullamcorper id,
+              interdum et nunc. Nulla facilisi. Fusce ullamcorper commodo
+              suscipit. Sed ante nulla, condimentum id libero vel, vulputate
+              condimentum arcu. Vestibulum porttitor, diam vitae placerat
+              aliquet, nisl neque pretium dolor, sed facilisis tellus tellus sed
+              turpis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Sed auctor suscipit libero in iaculis. Aenean ligula magna, tempor
+              lobortis hendrerit dictum, convallis ut sapien. Quisque id neque
+              id erat tempor dapibus non vel lectus.
+            </p>
+          </section>
+
+          <div className="recipe-img">
+            <Image
+              className="food-img"
+              src="/imgTeste.jpg"
+              alt="Teste"
+              layout="responsive"
+              width={500}
+              height={300}
+            />
+          </div>
+        </article>
+      </main>
+
+      <footer className="footer">
+        <p>&#xA9; Copyright 2025 Nathan Lourenço. All rights reserved.</p>
+      </footer>
+    </>
   );
 }
